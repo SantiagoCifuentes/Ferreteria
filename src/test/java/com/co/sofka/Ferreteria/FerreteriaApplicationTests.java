7908b58f750f809a33317d4d3ed8a8bc974350db
Santiago C <santiago.ci9619@gmail.com>
2022-03-30 11:29:17 -0500

    Vamo loco

---
+++ src/test/java/com/co/sofka/Ferreteria/FerreteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.sofka.Ferreteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FerreteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
